094d4f997464534d54bc82d7fa930347cf61e556
errerew <erlina31acc@gmail.com>
2026-03-13 16:51:53 +0700

    menambahkan code progrm untuk button insert di Program Studi menggunakan execute non Query

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -160,7 +160,25 @@ namespace PraktikumADO
 
         private void btnInsert_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "INSERT INTO ProgramStudi VALUES ('MI01', 'Manajemen Informatika')";
+
+                cmd = new SqlCommand(query, conn);
+
+                int hasil = cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Jumlah baris terpengaruh : " + hasil);
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
