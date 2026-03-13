d36a8928d4adf8d673537df115873063c0111272
errerew <erlina31acc@gmail.com>
2026-03-13 16:13:41 +0700

    menambahkan kode program button update dengan execute non query

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -91,7 +91,25 @@ namespace PraktikumADO
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Koneksi();
+                conn.Open();
 
+                string query = "UPDATE Mahasiswa SET Alamat='Yogyakarta' WHERE NIM='20240140121'";
+
+                cmd = new SqlCommand(query, conn);
+
+                int hasil = cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Jumlah baris terpengaruh : " + hasil);
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
